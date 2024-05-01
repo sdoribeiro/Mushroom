65f61764c3526f70646e53f403f46adf4ddc21c4
sdoribeiro <84580647+sdoribeiro@users.noreply.github.com>
2024-04-30 21:36:17 -0400

    correcao campos finance

--- MushRoomProject/app_yfinance.py
+++ MushRoomProject/app_yfinance.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
 ITSA4 = yf.Ticker("ITSA4.SA")
-print (ITSA4.info['ticker'])
-print (ITSA4.info['name'])
+
+print (ITSA4.info['shortName'])
 print (ITSA4.info['currentPrice'])
